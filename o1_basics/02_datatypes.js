30cae0010282fedba0ebb45f3b128e728bcd344a
Toolika Khare <144891151+toolikakhare28@users.noreply.github.com>
2025-05-13 12:12:31 +0000

    datatypes

---
+++ o1_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs , not browser
+//code readability should be high
+
+//primitive datatype
+
+let age = 18
+let isloggedin = false
+let state = null
+
+//number => 2 to poer 53
+//bigint
+//string
+//boolean=> true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof"Toolika"); //string
+console.log(typeof NULL); //object
+console.log(typeof undefined);//undefined
+
