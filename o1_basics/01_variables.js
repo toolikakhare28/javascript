8ae7b81b8e387bb0550e6c93ee4fce01772c1e1a
Toolika Khare <144891151+toolikakhare28@users.noreply.github.com>
2025-05-13 12:13:00 +0000

    datatype

---
+++ o1_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 12397698
+let accountemail = "toolika@google.com"
+var accountPassword = "4254654"
+accountCity = "jabalpur"
+let accountState;
+
+accountEmail = "tool@google.com"
+accountPassword = "3467653453"
+accountCity = "jaipur"
+
+/*
+prefer not to use var 
+because of issues in block scope and functional scope
+
+*/
+
+console.table([accountID,accountPassword , accountemail,accountState,accountCity ])
